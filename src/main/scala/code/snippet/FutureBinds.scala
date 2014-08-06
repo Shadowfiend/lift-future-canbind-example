6fce34ced34c874452575e295e3cb8e707dbb641
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2014-08-05 23:49:12 -0400

    Abstract future binding into one core method.

--- src/main/scala/code/snippet/FutureBinds.scala
+++ src/main/scala/code/snippet/FutureBinds.scala
@@ -11,29 +11,24 @@ import net.liftweb.util._
   import Helpers._
 
 object FutureBinds {
-  implicit def futureTransform[T](implicit innerTransform: CanBind[T], executionContext: ExecutionContext): CanBind[Future[T]] = new CanBind[Future[T]] {
-    def apply(future: => Future[T])(ns: NodeSeq): Seq[NodeSeq] = {
+  private def futureTransform[FutureType, T](innerTransform: CanBind[T], resolveFuture: (FutureType)=>T): CanBind[FutureType] = new CanBind[FutureType] {
+    def apply(future: => FutureType)(ns: NodeSeq): Seq[NodeSeq] = {
       val concreteFuture = future
       val snippetId = s"lazySnippet${Helpers.nextNum}"
 
-      S.mapSnippet(snippetId, { _: NodeSeq =>
-        innerTransform(Await.result(future, Duration.Inf))(ns).flatten
+      S.mapSnippet(snippetId, { ns: NodeSeq =>
+        innerTransform(resolveFuture(concreteFuture))(ns).flatten
       })
 
       <lift:lazy-load>{("^ [data-lift]" #> snippetId) apply ns}</lift:lazy-load>
     }
   }
 
-  implicit def lafutureTransform[T](implicit innerTransform: CanBind[T]): CanBind[LAFuture[T]] = new CanBind[LAFuture[T]] {
-    def apply(future: => LAFuture[T])(ns: NodeSeq): Seq[NodeSeq] = {
-      val concreteFuture = future
-      val snippetId = s"lazySnippet${Helpers.nextNum}"
+  implicit def futureTransform[T](implicit innerTransform: CanBind[T], executionContext: ExecutionContext): CanBind[Future[T]] = {
+    futureTransform[Future[T],T](innerTransform, (future) => Await.result(future, Duration.Inf))
+  }
 
-      S.mapSnippet(snippetId, { _: NodeSeq =>
-        innerTransform(future.get)(ns).flatten
-      })
-
-      <lift:lazy-load>{("^ [data-lift]" #> snippetId) apply ns}</lift:lazy-load>
-    }
+  implicit def lafutureTransform[T](implicit innerTransform: CanBind[T]): CanBind[LAFuture[T]] = {
+    futureTransform[LAFuture[T],T](innerTransform, (future) => future.get)
   }
 }
